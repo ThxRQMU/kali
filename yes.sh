3cebbb81312800a2320073e85d6710405e55b7ad
ThxRQMU <45893646+ThxRQMU@users.noreply.github.com>
2019-01-20 23:56:45 +0400

    Create yes.sh

---
+++ yes.sh
@@ -0,0 +1,6 @@
+pkg install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+cd Nethunter-In-Termux
+chmod -x kalinethunter (Если не работает)
+chmod 777 kalinethunter
+./kalinethunter
